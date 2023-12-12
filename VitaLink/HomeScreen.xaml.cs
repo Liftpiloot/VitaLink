df52247dae7082b02f42ae1664b850a6c37491bc
Abel <119590103+aAbel137@users.noreply.github.com>
2023-12-12 10:41:31 +0100

    Added login page

---
+++ VitaLink/HomeScreen.xaml.cs
@@ -0,0 +1,64 @@
+namespace VitaLink;
+
+public partial class HomeScreen : ContentPage
+{
+    public static int peopleButtonSize = 50;
+
+    public HomeScreen()
+    {
+        InitializeComponent();
+        // Set the theme to light
+        Application.Current.UserAppTheme = AppTheme.Light;
+
+        // Add the buttons to the stacklayout
+        Person kees = new Person(1, "Kees", "kees.jpg", 80);
+        Person peter = new Person(2, "Peter", "peter.jpg", 75);
+        List<Person> people = new List<Person>();
+        people.Add(kees);
+        people.Add(peter);
+
+        User user = User.getInstance();
+        user.Token = "123";
+        user.Username = "Abel";
+        user.FollowingList = people;
+
+        for (int i = 0; i < user.FollowingList.Count; i++)
+        {
+            peopleButtons.Children.Add(CreateFollowerButton(user.FollowingList[i]));
+        }
+    }
+    private Frame CreateFollowerButton(Person person)
+    {
+        // Create an image with the specified source
+        var imageButton = new ImageButton
+        {
+            Source = person.Image,
+            Aspect = Aspect.AspectFit,
+            WidthRequest = peopleButtonSize,
+            HeightRequest = peopleButtonSize
+        };
+        imageButton.Clicked += (sender, args) =>
+        {
+            showStats(person);
+            // TODO Show stats of the person
+        };
+
+        // Create a frame to wrap the image
+        var frame = new Frame
+        {
+            Content = imageButton,
+            HasShadow = true,
+            CornerRadius = 25, // Round the corners
+            WidthRequest = peopleButtonSize,
+            HeightRequest = peopleButtonSize,
+            Margin = 5
+        };
+
+        return frame;
+    }
+
+    private void showStats(Person person)
+    {
+        throw new NotImplementedException();
+    }
+}
--- VitaLink/MainPage.xaml.cs
+++ VitaLink/MainPage.xaml.cs
@@ -2,63 +2,20 @@
 {
     public partial class MainPage : ContentPage
     {
-        public static int peopleButtonSize = 50;
-
-        public MainPage() {
+        public MainPage()
+        {
             InitializeComponent();
             // Set the theme to light
             Application.Current.UserAppTheme = AppTheme.Light;
 
-            // Add the buttons to the stacklayout
-            Person kees = new Person(1, "Kees", "kees.jpg", 80);
-            Person peter = new Person(2, "Peter", "peter.jpg", 75);
-            List<Person> people = new List<Person>();
-            people.Add(kees);
-            people.Add(peter);
-
-            User user = User.getInstance();
-            user.Token = "123";
-            user.Username = "Abel";
-            user.FollowingList = people;
-
-            for (int i = 0; i < user.FollowingList.Count; i++)
+            // Go to homeScreen.xaml.cs when loginbutton is clicked
+            loginbutton.Clicked += (sender, args) =>
             {
-                peopleButtons.Children.Add(CreateFollowerButton(user.FollowingList[i]));
-            }
-        }
-        private Frame CreateFollowerButton(Person person)
-        {
-            // Create an image with the specified source
-            var imageButton = new ImageButton
-            {
-                Source = person.Image,
-                Aspect = Aspect.AspectFit,
-                WidthRequest = peopleButtonSize,
-                HeightRequest = peopleButtonSize
-            };
-            imageButton.Clicked += (sender, args) =>
-            {
-                showStats(person);
-                // TODO Show stats of the person
+                // TODO Handle login
+                Navigation.PushAsync(new HomeScreen());
             };
 
-            // Create a frame to wrap the image
-            var frame = new Frame
-            {
-                Content = imageButton,
-                HasShadow = true,
-                CornerRadius = 25, // Round the corners
-                WidthRequest = peopleButtonSize,
-                HeightRequest = peopleButtonSize,
-                Margin = 5
-            };
 
-            return frame;
-        }
-
-        private void showStats(Person person)
-        {
-            throw new NotImplementedException();
         }
     }
 }
