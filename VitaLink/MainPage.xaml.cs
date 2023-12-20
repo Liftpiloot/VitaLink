5c1011e66167e46beb95e5d1b9a0675744c7b2cf
Quincy Heijblom <522730@student.fontys.nl>
2023-12-20 15:26:20 +0100

    made login logic

--- VitaLink/MainPage.xaml.cs
+++ VitaLink/MainPage.xaml.cs
@@ -1,4 +1,9 @@
-﻿namespace VitaLink
+﻿using System.Net.Http.Headers;
+using System.Security.Cryptography.X509Certificates;
+using System.Net.Http.Headers;
+using System.Text.Json.Nodes;
+
+namespace VitaLink
 {
     public partial class MainPage : ContentPage
     {
@@ -11,8 +16,14 @@
             // Go to homeScreen.xaml.cs when loginbutton is clicked
             LoginButton.Clicked += (sender, args) =>
             {
+                string email = EmailEntry.Text;
+                string password = PasswordEntry.Text;
+                loginAsync(email, password);
                 // TODO Handle login
-                Navigation.PushAsync(new HomeScreen());
+
+                
+
+               
             };
             // Go to register.xaml.cs when registerbutton is clicked
             RegisterButton.Clicked += (sender, args) =>
@@ -21,7 +32,34 @@
                 Navigation.PushAsync(new RegisterPage());
             };
 
+        }
+        public async Task loginAsync(string email, string password)
+        {
+            var client = new HttpClient();
+            var request = new HttpRequestMessage
+            {
+                Method = HttpMethod.Post,
+                RequestUri = new Uri("https://vita-link.nl/api/v1/login"),
+                Headers =
+                {
+                    { "Accept", "application/json" },
+                },
+                Content = new StringContent("{\n  \"email\": \"" + email + "\",\n  \"password\": \"" + password + "\"\n}")
 
+                {
+                    Headers =
+        {
+            ContentType = new MediaTypeHeaderValue("application/json")
+        }
+                }
+            };
+            using (var response = await client.SendAsync(request))
+            {
+                response.EnsureSuccessStatusCode();
+                var body = await response.Content.ReadAsStringAsync();
+                Console.WriteLine(body);
+            }
+            Navigation.PushAsync(new HomeScreen());
         }
     }
 }
